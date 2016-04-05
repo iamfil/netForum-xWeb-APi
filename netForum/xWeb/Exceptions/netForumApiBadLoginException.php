07442f40fdab7f0ff379ad7fdc5d9a0157dfd8ea
Filipe Ferreira <filipe.ferreira@higherbits.com>
2016-04-05 15:06:04 -0400

    Clean master branch, moved everything over to 7.x-1.x branch

--- netForum/xWeb/Exceptions/netForumApiBadLoginException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace netForum\xWeb\Exceptions;
-
-class netForumApiBadLoginException extends netForumApiException {
-  public function __construct($message = "", $code = 0, \Exception $previous = NULL) {
-    parent::__construct($message, $code, $previous);
-  }
-}
--- netForum/xWeb/Xml/Exceptions/InvalidTokenException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace netForum\xWeb\Xml\Exceptions;
-
-class InvalidTokenException extends netForumException {
-  public function __construct($message = "", $code = 0, \Exception $previous = NULL) {
-    parent::__construct($message, $code, $previous);
-  }
-}
